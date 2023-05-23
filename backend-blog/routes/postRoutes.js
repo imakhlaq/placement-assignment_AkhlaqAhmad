a2ec025634081cf9dae137f76754df2d22a3d089
imakhlaq <imakhlaqXD@gmail.com>
2023-05-23 15:29:13 +0530

    more projects added

--- backend-blog/routes/postRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import {createPosts, deletePost, getPosts, updatePost} from "../controllers/postControllers.js";
-const router = express.Router()
-
-router.route("/").get(getPosts).post(createPosts)
-router.route("/:id").put(updatePost).delete(deletePost)
-
-export default router
